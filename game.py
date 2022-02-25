9479bb968f99f67783b56257273fcff4d22568f1
ARyback <adam.ryback@gmail.com>
2022-02-24 21:16:38 -0600

    Second main draft completed. Project is basically MVP.

--- game.py
+++ game.py
@@ -9,67 +9,84 @@ class Game:
         pass
 
     def run_game(self):
-        self.player_one = Human(input("Please enter your name: "))
-        print(self.player_one.name)
-        self.player_two = AI("Computer player")
-        print(self.player_two.name)
-        print(f"This game will face off {self.player_one.name} versus {self.player_two.name}!")
-        self.faceoff(self.player_one, self.player_two)
-        print(self.player_one.gesture_selected)
-        print(self.player_two.gesture_selected)
-        self.victory(self.player_one, self.player_two)
+        self.player_choice()
+        self.victory()
         pass
 
-    def faceoff(self, player_one, player_two):
+    def player_choice(self):
+        user_input = input("Enter 1 for human vs human, enter 2 for human vs AI, and enter 3 for AI vs AI: ")
+        if user_input == "1": 
+            self.player_one = Human(input("Please enter your name: "))
+            self.player_two = Human(input("Please enter your name: "))
+            print(f"This game will face off {self.player_one.name} versus {self.player_two.name}!")
+        elif user_input == "2":
+            self.player_one = Human(input("Please enter your name: "))
+            self.player_two = AI("Computer")
+            print(f"This game will face off {self.player_one.name} versus {self.player_two.name}!")
+        elif user_input == "3":
+            self.player_one = AI("Computer 1")
+            self.player_two = AI("Computer 2")
+            print(f"This game will face off {self.player_one.name} versus {self.player_two.name}!")
+        else:
+            self.player_choice()
+        pass
+
+    def faceoff(self):
         self.player_one.choose_gesture()
+        print(self.player_one.gesture_selected)
         self.player_two.choose_gesture()
+        print(self.player_two.gesture_selected)
+        self.comparison(self.player_one, self.player_two)
         pass
 
-    def victory(self, player_one, player_two):
+    def victory(self):
         while self.player_one.score < 2 and self.player_two.score < 2:
-            self.comparison_p1_winner(player_one,player_two)
-            self.comparison_p2_winner(player_one,player_two)
+            self.faceoff()
+        if self.player_one.score == 2:
+            print(f"{self.player_one.name} wins!")
+        else:
+            print(f"{self.player_two.name} wins!")
         pass
             
-    def comparison_p1_winner(self, player_one, player_two):
-        if self.player_one.gesture_selected == self.player_one.gesture_list[0] and (self.player_two.gesture_selected == self.player_two.gesture_list[2] or self.player_two.gesture_selected == self.player_two.gesture_list[3]):
-            self.player_one.score += 1
-            print(f"{self.player_one.gesture_selected} wins this round!")
-        elif self.player_one.gesture_selected == self.player_one.gesture_list[1] and (self.player_two.gesture_selected == self.player_two.gesture_list[0] or self.player_two.gesture_selected == self.player_two.gesture_list[4]):
-            self.player_one.score += 1
-            print(f"{self.player_one.gesture_selected} wins this round!")
-        elif self.player_one.gesture_selected == self.player_one.gesture_list[2] and (self.player_two.gesture_selected == self.player_two.gesture_list[1] or self.player_two.gesture_selected == self.player_two.gesture_list[3]):
-            self.player_one.score += 1
-            print(f"{self.player_one.gesture_selected} wins this round!")
-        elif self.player_one.gesture_selected == self.player_one.gesture_list[3] and (self.player_two.gesture_selected == self.player_two.gesture_list[4] or self.player_two.gesture_selected == self.player_two.gesture_list[1]):
-            self.player_one.score += 1
-            print(f"{self.player_one.gesture_selected} wins this round!")
-        elif self.player_one.gesture_selected == self.player_one.gesture_list[4] and (self.player_two.gesture_selected == self.player_two.gesture_list[2] or self.player_two.gesture_selected == self.player_two.gesture_list[0]):
-            self.player_one.score += 1 
-            print(f"{self.player_one.gesture_selected} wins this round!")
+    def comparison(self, x_player, y_player):
+        if x_player.gesture_selected == y_player.gesture_selected:
+            print("It's a tie! Throw again!")
+        elif x_player.gesture_selected == x_player.gesture_list[0]:
+            if y_player.gesture_selected == y_player.gesture_list[2] or y_player.gesture_selected == y_player.gesture_list[3]:
+                x_player.score += 1
+                print(f"{x_player.gesture_selected} wins this round!")
+            else:
+                y_player.score += 1
+                print(f"{y_player.gesture_selected} wins this round!")
+        elif x_player.gesture_selected == x_player.gesture_list[1]:
+            if y_player.gesture_selected == y_player.gesture_list[0] or y_player.gesture_selected == y_player.gesture_list[4]:
+                x_player.score += 1
+                print(f"{x_player.gesture_selected} wins this round!")
+            else:
+                y_player.score += 1
+                print(f"{y_player.gesture_selected} wins this round!")
+        elif x_player.gesture_selected == x_player.gesture_list[2]:
+            if y_player.gesture_selected == y_player.gesture_list[1] or y_player.gesture_selected == y_player.gesture_list[3]:
+                x_player.score += 1
+                print(f"{x_player.gesture_selected} wins this round!")
+            else:
+                y_player.score += 1
+                print(f"{y_player.gesture_selected} wins this round!")
+        elif x_player.gesture_selected == x_player.gesture_list[3]:
+            if y_player.gesture_selected == y_player.gesture_list[4] or y_player.gesture_selected == y_player.gesture_list[1]:
+                x_player.score += 1
+                print(f"{x_player.gesture_selected} wins this round!")
+            else:
+                y_player.score += 1
+                print(f"{y_player.gesture_selected} wins this round!")
+        elif x_player.gesture_selected == x_player.gesture_list[4]:
+            if y_player.gesture_selected == y_player.gesture_list[2] or y_player.gesture_selected == y_player.gesture_list[0]:
+                x_player.score += 1
+                print(f"{x_player.gesture_selected} wins this round!")
+            else:
+                y_player.score += 1
+                print(f"{y_player.gesture_selected} wins this round!")
         else:
             pass
         pass
 
-    def comparison_p2_winner(self, player_one, player_two):
-        if self.player_two.gesture_selected == self.player_two.gesture_list[0] and (self.player_one.gesture_selected == self.player_one.gesture_list[2] or self.player_one.gesture_selected == self.player_one.gesture_list[3]):
-            self.player_two.score += 1
-            print(f"{self.player_two.gesture_selected} wins this round!")
-        elif self.player_two.gesture_selected == self.player_two.gesture_list[1] and (self.player_one.gesture_selected == self.player_one.gesture_list[0] or self.player_one.gesture_selected == self.player_one.gesture_list[4]):
-            self.player_two.score += 1
-            print(f"{self.player_two.gesture_selected} wins this round!")
-        elif self.player_two.gesture_selected == self.player_two.gesture_list[2] and (self.player_one.gesture_selected == self.player_one.gesture_list[1] or self.player_one.gesture_selected == self.player_one.gesture_list[3]):
-            self.player_two.score += 1
-            print(f"{self.player_two.gesture_selected} wins this round!")
-        elif self.player_two.gesture_selected == self.player_two.gesture_list[3] and (self.player_one.gesture_selected == self.player_one.gesture_list[4] or self.player_one.gesture_selected == self.player_one.gesture_list[1]):
-            self.player_two.score += 1
-            print(f"{self.player_two.gesture_selected} wins this round!")
-        elif self.player_two.gesture_selected == self.player_two.gesture_list[4] and (self.player_one.gesture_selected == self.player_one.gesture_list[2] or self.player_one.gesture_selected == self.player_one.gesture_list[0]):
-            self.player_two.score += 1
-            print(f"{self.player_two.gesture_selected} wins this round!")
-        else:
-            pass
-
-
-new_game = Game()
-new_game.run_game()  
--- human.py
+++ human.py
@@ -3,29 +3,24 @@ from player import Player
 class Human(Player):
     def __init__(self, name):
         super().__init__(name)
-        self.gesture_selected = None  
+        self.gesture_selected = None
 
     def choose_gesture(self):
-        user_choice = input(f"Press 0 for Rock, 1 for Paper, 2 for Scissors, 3 for Lizard, and 4 for Spock!")
+        user_choice = input(f"Press 0 for {self.gesture_list[0]}, 1 for {self.gesture_list[1]}, 2 for {self.gesture_list[2]}, 3 for {self.gesture_list[3]}, and 4 for {self.gesture_list[4]}!")
         while user_choice != "0" and user_choice != "1" and user_choice != "2" and user_choice != "3" and user_choice != "4":
-            user_choice = input(f"Oops! Try again! Press 0 for Rock, 1 for Paper, 2 for Scissors, 3 for Lizard, and 4 for Spock!")
-        if user_choice == "0":
-            user_choice = int(user_choice)
-            self.gesture_selected = self.gesture_list[user_choice]
-        elif user_choice == "1":
-            user_choice = int(user_choice)
-            self.gesture_selected = self.gesture_list[user_choice]
-        elif user_choice == "2":
-            user_choice = int(user_choice)
-            self.gesture_selected = self.gesture_list[user_choice]
-        elif user_choice == "3":
-            user_choice = int(user_choice)
-            self.gesture_selected = self.gesture_list[user_choice]
-        elif user_choice == "4":
-            user_choice = int(user_choice)
-            self.gesture_selected = self.gesture_list[user_choice]
-        else:
-            print("There was an error~!")
+            user_choice = input(f"Oops! Try again! Press 0 for {self.gesture_list[0]}, 1 for {self.gesture_list[1]}, 2 for {self.gesture_list[2]}, 3 for {self.gesture_list[3]}, and 4 for {self.gesture_list[4]}!")
+        self.gesture_selected = self.gesture_list[int(user_choice)]
+        # if user_choice == "0":
+        # elif user_choice == "1":
+        #     self.gesture_selected = self.gesture_list[int(user_choice)]
+        # elif user_choice == "2":
+        #     self.gesture_selected = self.gesture_list[int(user_choice)]
+        # elif user_choice == "3":
+        #     self.gesture_selected = self.gesture_list[int(user_choice)]
+        # elif user_choice == "4":
+        #     self.gesture_selected = self.gesture_list[int(user_choice)]
+        # else:
+        #     print("There was an error~!")
         pass
 
 # new = Human('fred')
